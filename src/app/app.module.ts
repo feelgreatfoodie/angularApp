214f9bc2cb2746700a20dac10411d1d3633ead25
Christian Bourlier <chrisbourlier@hotmail.com>
2018-10-11 07:34:46 -0600

    posts component/service & http get added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/Forms'
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component'
 import { UserComponent } from './components/user/user.componenet'
 import { UsersComponent } from './components/users/users.component'
 import { NavbarComponent } from './components/navbar/navbar.component'
-import { DataService } from './services/data.service'
+
+import { UserService } from './services/user.service'
+import { PostService } from './services/post.service'
+import { PostsComponent } from './components/posts/posts.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     UsersComponent,
-    NavbarComponent
+    NavbarComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule, 
+    HttpClientModule
   ],
-  providers: [DataService],
+  providers: [
+    UserService, 
+    PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
